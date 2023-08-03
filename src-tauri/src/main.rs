f4a617c5051d24cf2f05096009057b2909ba5716
altacountbabi <radustefan379@gmail.com>
2023-08-03 22:04:35 +0300

    should hide powershell window now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,26 +4,29 @@ use tauri::WindowEvent;
 use tauri::AppHandle;
 use tauri::generate_handler;
 use tauri::Manager;
+use std::os::windows::process::CommandExt;
 use std::process::{Command, Stdio};
+use winapi::um::winbase::CREATE_NO_WINDOW;
 use std::fs;
 
 #[tauri::command]
-fn run_ps(app: AppHandle, code: &str) {
-    let output = Command::new("powershell")
-        .args(&[
-            "-NoProfile",
-            "-ExecutionPolicy",
-            "Bypass",
-            "-Command",
-            code
-        ])
-        .stdout(Stdio::piped())
-        .output()
-        .expect("Failed to run PowerShell command.");
+    fn run_ps(app: AppHandle, code: &str) {
+        let output = Command::new("powershell")
+            .args(&[
+                "-NoProfile",
+                "-ExecutionPolicy",
+                "Bypass",
+                "-Command",
+                code
+            ])
+            .stdout(Stdio::piped())
+            .creation_flags(CREATE_NO_WINDOW)
+            .output()
+            .expect("Failed to run PowerShell command.");
 
-    let output_string = String::from_utf8_lossy(&output.stdout);
-    app.emit_all("ps_output", output_string).unwrap();
-}
+        let output_string = String::from_utf8_lossy(&output.stdout);
+        app.emit_all("ps_output", output_string).unwrap();
+    }
 
 #[tauri::command]
 fn remove_file(file_path: &str) {
